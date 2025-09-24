e06804b2f35ff6a6d47b245e408e939142d8b998
david.niwewe <davejuelz@gmail.com>
2025-09-24 10:55:26 +0200

    Fixed showing more details

--- src/ui-component/LongTextCell.js
+++ src/ui-component/LongTextCell.js
@@ -11,7 +11,7 @@ import {
 import CloseIcon from '@mui/icons-material/Close';
 import formatTitle from 'utils/title-formatter';
 
-const MAX_LENGTH = 100; // Number of visible characters before truncation
+const MAX_LENGTH = 100;
 
 export const LongTextCell = ({ attribute, value }) => {
   const [open, setOpen] = useState(false);
@@ -19,27 +19,25 @@ export const LongTextCell = ({ attribute, value }) => {
   const cleanHTML = DOMPurify.sanitize(value || '');
   const truncatedHTML = truncate(cleanHTML, MAX_LENGTH);
 
-  const isTruncated = cleanHTML.length > truncatedHTML.length;
-
   return (
     <>
       <div
         style={{ maxWidth: 300, wordBreak: 'break-word' }}
+        dangerouslySetInnerHTML={{ __html: truncatedHTML }}
       />
-      {isTruncated && (
-        <Typography
-          variant="body2"
-          color="primary"
-          onClick={() => setOpen(true)}
-          style={{ cursor: 'pointer', marginTop: 4 }}
-        >
-          Click To Read
-        </Typography>
-      )}
+
+      <Typography
+        variant="body2"
+        color="primary"
+        onClick={() => setOpen(true)}
+        style={{ cursor: 'pointer', marginTop: 4 }}
+      >
+        View Details
+      </Typography>
 
       <Dialog open={open} onClose={() => setOpen(false)} fullWidth maxWidth="sm">
         <DialogTitle>
-          Full {formatTitle (attribute.name)}
+          Full {formatTitle(attribute.name)}
           <IconButton
             onClick={() => setOpen(false)}
             sx={{ position: 'absolute', right: 8, top: 8 }}
